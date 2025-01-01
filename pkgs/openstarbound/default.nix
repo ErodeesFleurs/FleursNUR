aeb5f5d49902af838cae20c26de325cdbb334be7
ErodeesFleurs <862959461@qq.com>
2025-01-01 13:09:06 +0800

    添加启动配置

--- pkgs/openstarbound/default.nix
+++ pkgs/openstarbound/default.nix
@@ -1,58 +1,61 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  autoPatchelfHook,
-  unzip,
-  libGL,
-  libGLU,
-  libSM,
-  libICE,
-  libX11,
-  libXext,
-  libgcc,
+  writeShellApplication,
   ...
 }:
 
-stdenv.mkDerivation rec {
-  name = "OpenStarbound-${version}";
-  version = "Nightly";
-  src = fetchurl {
-    url = "https://nightly.link/OpenStarbound/OpenStarbound/workflows/build/main/OpenStarbound-Linux-Client.zip";
-    sha256 = "sha256-KARChrAZHrVPcGZb4Mkt6oCfQb5V/T+wU/8Qi/L8zHQ=";
+let
+  openstarbound-raw = import ./openstarbound.nix {
+    inherit
+      lib
+      stdenv
+      fetchurl
+      autoPatchelfHook
+      makeWrapper
+      unzip
+      libGL
+      libGLU
+      libSM
+      libICE
+      libX11
+      libXext
+      libgcc
+      ;
   };
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-    unzip
-
-    # Required libraries
-    libGL
-    libGLU
-    libSM
-    libICE
-    libX11
-    libXext
-    libgcc
+in
+writeShellApplication {
+  name = "openstarbound-${openstarbound-raw.version}";
+  runtimeInputs = [
+    openstarbound-raw
   ];
+  text = ''
+    steam_assets_dir="$HOME/.local/share/Steam/steamapps/common/Starbound/assets"
+    storage_dir="$HOME/.local/share/xStarbound/storage"
+    log_dir="$HOME/.local/share/xStarbound/logs"
+    mod_dir="$HOME/.local/share/xStarbound/mods"
 
-  unpackPhase = ''
-    # Unzip the downloaded file
-    mkdir -p $TMPDIR/build
-    unzip $src -d $TMPDIR/build
+    mkdir -p "$storage_dir"
+    tmp_cfg="$(mktemp -t openstarbound.XXXXXXXX)"
 
-    # Extract client.tar
-    tar -xvf $TMPDIR/build/client.tar -C $TMPDIR/build
+    cat << EOF > "$tmp_cfg"
+      {
+      "assetDirectories" : [
+        "$gog_assets_dir",
+        "$steam_assets_dir",
+        "$mod_dir",
+        "../assets/"
+      ],
+
+      "storageDirectory" : "$storage_dir",
+      "logDirectory" : "$log_dir"
+    }
+    EOF
+
+    exec ${openstarbound-raw}/linux/starbound \
+      -bootconfig "$tmp_cfg" \
+      "$@"
+
+    rm "$tmp_cfg"
   '';
-  installPhase = ''
-    mkdir -p $out/{linux, assets}
-
-    cp -r $TMPDIR/build/client_distribution/linux $out
-    cp -r $TMPDIR/build/client_distribution/assets $out
-  '';
-
-  meta = {
-    description = "OpenStarbound is a free open-source Starbound server implementation";
-    homepage = "https://github.com/OpenStarbound/OpenStarbound";
-  };
 }
---
+++ pkgs/openstarbound/openstarbound.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  makeWrapper,
+  unzip,
+  libGL,
+  libGLU,
+  libSM,
+  libICE,
+  libX11,
+  libXext,
+  libgcc,
+  ...
+}:
+
+stdenv.mkDerivation rec {
+  name = "OpenStarbound-${version}";
+  version = "Nightly";
+  src = fetchurl {
+    url = "https://nightly.link/OpenStarbound/OpenStarbound/workflows/build/main/OpenStarbound-Linux-Client.zip";
+    sha256 = "sha256-KARChrAZHrVPcGZb4Mkt6oCfQb5V/T+wU/8Qi/L8zHQ=";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    makeWrapper
+    unzip
+
+    # Required libraries
+    libGL
+    libGLU
+    libSM
+    libICE
+    libX11
+    libXext
+    libgcc
+  ];
+
+  unpackPhase = ''
+    # Unzip the downloaded file
+    mkdir -p $TMPDIR/build
+    unzip $src -d $TMPDIR/build
+
+    # Extract client.tar
+    tar -xvf $TMPDIR/build/client.tar -C $TMPDIR/build
+  '';
+  installPhase = ''
+    mkdir -p $out/{linux, assets, bin}
+
+    cp -r $TMPDIR/build/client_distribution/linux $out
+    cp -r $TMPDIR/build/client_distribution/assets $out
+  '';
+
+  meta = {
+    description = "OpenStarbound is a free open-source Starbound server implementation";
+    homepage = "https://github.com/OpenStarbound/OpenStarbound";
+  };
+}
