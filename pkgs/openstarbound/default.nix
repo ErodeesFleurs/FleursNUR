aa144b3234d8eecd1ef40caa56c07206a7b97c45
ErodeesFleurs <862959461@qq.com>
2025-01-05 21:55:33 +0800

    oops

--- pkgs/openstarbound/default.nix
+++ pkgs/openstarbound/default.nix
@@ -34,7 +34,7 @@ let
       version = "Nightly";
       src = fetchurl {
         url = "https://nightly.link/OpenStarbound/OpenStarbound/workflows/build/main/OpenStarbound-Linux-Client.zip";
-        sha256 = "sha256-KARChrAZHrVPcGZb4Mkt6oCfQb5V/T+wU/8Qi/L8zHQ=";
+        sha256 = "sha256-4XON3e9mE7FkismzBmuDSMHwUsXpnjZQSvwER7vsa9U=";
       };
 
       nativeBuildInputs = [
