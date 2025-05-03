3b14ba96086a5eaf5a2a5f6a01542ca41ef82c2a
ErodeesFleurs <862959461@qq.com>
2025-05-03 11:42:34 +0800

    temp remove xmcl

--- default.nix
+++ default.nix
@@ -17,8 +17,6 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   openstarbound = pkgs.callPackage ./pkgs/openstarbound { };
-  xmcl = pkgs.callPackage ./pkgs/xmcl { };
+  # xmcl = pkgs.callPackage ./pkgs/xmcl { };
   dingtalk = pkgs.callPackage ./pkgs/dingtalk { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
 }
